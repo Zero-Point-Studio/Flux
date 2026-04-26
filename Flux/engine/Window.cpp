6e58bc4cdd6b1e98d76918d4b73dfafaff02ab2f
Idkthisguy <idkf0ul@gmail.com>
2026-04-26 08:16:31 -0400

    WOOO LINUX TESTING

--- Flux/engine/Window.cpp
+++ Flux/engine/Window.cpp
@@ -20,161 +20,176 @@
 #include "Window.h"
 #include <iostream>
 
+#if defined(_WIN32)
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 #include <dwmapi.h>
-
 #pragma comment(lib, "dwmapi.lib")
+#elif defined(__linux__)
+#define GLFW_EXPOSE_NATIVE_X11
+#include <GLFW/glfw3native.h>
+#endif
 
 #include "imgui.h"
 #include "imgui_internal.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-namespace Flux {
-	Window::Window(int width, int height, const std::string& title)
-		: m_width(width), m_height(height), m_title(title)
-	{
-		if (!glfwInit()) {
-			std::cerr << "ERROR: FAILED TO INITIALIZE GLFW" << std::endl;
-		}
+namespace Flux
+{
+    Window::Window(int width, int height, const std::string& title)
+        : m_width(width), m_height(height), m_title(title)
+    {
+        if (!glfwInit())
+        {
+            std::cerr << "ERROR: FAILED TO INITIALIZE GLFW" << std::endl;
+        }
 
-		m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL);
+        m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL);
 
-		HWND hwnd = glfwGetWin32Window(m_window);
+#if defined(_WIN32)
+        HWND hwnd = glfwGetWin32Window(m_window);
+        BOOL useDarkMode = TRUE;
+        DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &useDarkMode, sizeof(useDarkMode));
+#endif
 
-		BOOL useDarkMode = TRUE;
+        if (!m_window)
+        {
+            std::cerr << "ERROR: FAILED TO CREATE WINDOW" << std::endl;
+        }
 
-		DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &useDarkMode, sizeof(useDarkMode));
+        glfwMakeContextCurrent(m_window);
 
-		if (!m_window) {
-			std::cerr << "ERROR: FAILED TO CREATE WINDOW" << std::endl;
-		}
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cerr << "ERROR: FAILED TO INITIALIZE GLAD" << std::endl;
+        }
 
-		glfwMakeContextCurrent(m_window);
+        GLFWimage images[1];
+        images[0].pixels = stbi_load("assets/icon.png", &images[0].width, &images[0].height, 0, 4);
 
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-			std::cerr << "ERROR: FAILED TO INITIALIZE GLAD" << std::endl;
-		}
+        if (images[0].pixels)
+        {
+            glfwSetWindowIcon(m_window, 1, images);
+            stbi_image_free(images[0].pixels);
+        }
 
-		GLFWimage images[1];
-		images[0].pixels = stbi_load("assets/icon.png", &images[0].width, &images[0].height, 0, 4);
+        m_viewport.Init();
 
-		if (images[0].pixels) {
-			glfwSetWindowIcon(m_window, 1, images);
-			stbi_image_free(images[0].pixels);
-		}
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
 
-		m_viewport.Init();
-
-		IMGUI_CHECKVERSION();
-		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-
-		
-		
-		ImGui_ImplGlfw_InitForOpenGL(m_window, true);
-		ImGui_ImplOpenGL3_Init("#version 460");
-	}
-
-	Window::~Window()
-	{
-		glfwDestroyWindow(m_window);
-		glfwTerminate();
-	}
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
 
-	bool Window::shouldClose() const
-	{
-		return glfwWindowShouldClose(m_window);
-	}
+        ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+        ImGui_ImplOpenGL3_Init("#version 450");
+    }
 
-	void Window::update() // Main render
-	{
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		ImGuiViewport* viewport = ImGui::GetMainViewport();
-
-		ImVec2 dockPos = viewport->Pos;
-		dockPos.y += 55.0f;
-
-		ImVec2 dockSize = viewport->Size;
-		dockSize.y -= 55.0f;
-
-		static bool firstTime = true;
-		ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
-
-		ImGui::SetNextWindowPos(dockPos);
-		ImGui::SetNextWindowSize(dockSize);
-		ImGui::SetNextWindowViewport(viewport->ID);
-
-		ImGuiWindowFlags host_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-			ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-			ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
-			ImGuiWindowFlags_NoBackground;
-
-		ImGui::Begin("MainDockHost", nullptr, host_flags);
-
-		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
-
-		if (firstTime) {
-			firstTime = false;
-
-			ImGui::DockBuilderRemoveNode(dockspace_id);
-			ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
-			ImGui::DockBuilderSetNodeSize(dockspace_id, dockSize);
-
-			ImGuiID dock_id_left;
-			ImGuiID dock_id_right;
-			ImGuiID dock_id_bottom;
-			ImGuiID dock_id_bottomRight;
-			ImGuiID dock_id_center = dockspace_id;
-
-			dock_id_left = ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Left, 0.32f, nullptr, &dock_id_center);
-			dock_id_bottom = ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Down, 0.25f, nullptr, &dock_id_center);
-			dock_id_right = ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Right, 0.32f, nullptr, &dock_id_center);
-			dock_id_bottomRight = ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Down, 0.5f, nullptr, &dock_id_right);
-
-			ImGui::DockBuilderDockWindow("Viewport", dock_id_center);
-			ImGui::DockBuilderDockWindow("Explorer", dock_id_right);
-			ImGui::DockBuilderDockWindow("Output", dock_id_bottom);
-			ImGui::DockBuilderDockWindow("Properties", dock_id_bottomRight);
-			ImGui::DockBuilderDockWindow("Heiarchy", dock_id_left);
+    Window::~Window()
+    {
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
+    }
 
 
-			ImGui::DockBuilderFinish(dockspace_id);
-		}
-		ImGui::End();
+    bool Window::shouldClose() const
+    {
+        return glfwWindowShouldClose(m_window);
+    }
 
-		m_viewport.RenderViewport(m_heiarchy);
-		m_explorer.renderExplorer(m_viewport);
-		m_ribbon.renderRibbon();
-		m_output.renderOutput();
-		m_properties.renderProperties(&m_heiarchy);
-		m_heiarchy.renderHeiarchy(m_viewport.activeProjectPath);
+    void Window::update() // Main render
+    {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
 
-		if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-			GLFWwindow* backup_current_context = glfwGetCurrentContext();
-			ImGui::UpdatePlatformWindows();
-			ImGui::RenderPlatformWindowsDefault();
-			glfwMakeContextCurrent(backup_current_context);
-		}
+        ImVec2 dockPos = viewport->Pos;
+        dockPos.y += 55.0f;
 
-		glfwPollEvents();
-		glfwSwapBuffers(m_window);
-	}
+        ImVec2 dockSize = viewport->Size;
+        dockSize.y -= 55.0f;
 
-	void Window::clear(float r, float g, float b, float a) {
-		glClearColor(r, g, b, a);
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
+        static bool firstTime = true;
+        ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
+
+        ImGui::SetNextWindowPos(dockPos);
+        ImGui::SetNextWindowSize(dockSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+
+        ImGuiWindowFlags host_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
+            ImGuiWindowFlags_NoBackground;
+
+        ImGui::Begin("MainDockHost", nullptr, host_flags);
+
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
+
+        if (firstTime)
+        {
+            firstTime = false;
+
+            ImGui::DockBuilderRemoveNode(dockspace_id);
+            ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+            ImGui::DockBuilderSetNodeSize(dockspace_id, dockSize);
+
+            ImGuiID dock_id_left;
+            ImGuiID dock_id_right;
+            ImGuiID dock_id_bottom;
+            ImGuiID dock_id_bottomRight;
+            ImGuiID dock_id_center = dockspace_id;
+
+            dock_id_left = ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Left, 0.32f, nullptr, &dock_id_center);
+            dock_id_bottom =
+                ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Down, 0.25f, nullptr, &dock_id_center);
+            dock_id_right =
+                ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Right, 0.32f, nullptr, &dock_id_center);
+            dock_id_bottomRight = ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Down, 0.5f, nullptr,
+                                                              &dock_id_right);
+
+            ImGui::DockBuilderDockWindow("Viewport", dock_id_center);
+            ImGui::DockBuilderDockWindow("Explorer", dock_id_right);
+            ImGui::DockBuilderDockWindow("Output", dock_id_bottom);
+            ImGui::DockBuilderDockWindow("Properties", dock_id_bottomRight);
+            ImGui::DockBuilderDockWindow("Heiarchy", dock_id_left);
+
+
+            ImGui::DockBuilderFinish(dockspace_id);
+        }
+        ImGui::End();
+
+        m_viewport.RenderViewport(m_heiarchy);
+        m_explorer.renderExplorer(m_viewport);
+        m_ribbon.renderRibbon();
+        m_output.renderOutput();
+        m_properties.renderProperties(&m_heiarchy);
+        m_heiarchy.renderHeiarchy(m_viewport.activeProjectPath);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+        glfwPollEvents();
+        glfwSwapBuffers(m_window);
+    }
+
+    void Window::clear(float r, float g, float b, float a)
+    {
+        glClearColor(r, g, b, a);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 }
